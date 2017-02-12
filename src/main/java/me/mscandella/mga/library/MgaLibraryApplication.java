f68ccbb2a74432f19255f0e14d6df294d3777445
Maria Gomez <mgomez@thoughtworks.com>
2017-02-12 11:36:43 -0500

    Created monolith

---
+++ src/main/java/me/mscandella/mga/library/MgaLibraryApplication.java
@@ -0,0 +1,12 @@
+package me.mscandella.mga.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgaLibraryApplication.class, args);
+	}
+}
---
+++ src/main/java/me/mscandella/mga/library/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package me.mscandella.mga.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Maria");
+        return "Hello";
+    }
+}
