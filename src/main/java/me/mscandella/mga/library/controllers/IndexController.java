e91234fd475cca8dee34d2d576cd0d99d1646c5d
Maria Gomez <mgomez@thoughtworks.com>
2017-02-12 20:05:58 -0500

    Added Controller for root and redirect to Catalog

---
+++ src/main/java/me/mscandella/mga/library/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package me.mscandella.mga.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/catalog";
+    }
+
+}
