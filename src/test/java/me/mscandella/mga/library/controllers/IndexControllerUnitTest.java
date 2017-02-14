21b0430f7b8e6b224a5eb9ab1fc1def8d86a2303
Maria Gomez <mgomez@thoughtworks.com>
2017-02-14 18:39:11 -0500

    Added unit test for index controller

---
+++ src/test/java/me/mscandella/mga/library/controllers/IndexControllerUnitTest.java
@@ -0,0 +1,16 @@
+package me.mscandella.mga.library.controllers;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class IndexControllerUnitTest {
+    
+    @Test
+    public void shouldReturnRedirectToCatalog() throws Exception {
+        String resultView = new IndexController().index();
+        assertThat(resultView, is("redirect:/catalog"));
+    }
+}
